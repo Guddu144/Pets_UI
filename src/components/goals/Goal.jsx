5d5c6e3bc26ec16dc8fec468154b0fcf8685e6ab
pawanrijal <pwnrijal@gmail.com>
2023-06-10 22:08:39 +0545

    create fixed

--- src/components/goals/Goal.jsx
+++ src/components/goals/Goal.jsx
@@ -1,56 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import { Modal, PageHeader, PageLayout } from '../common';
-import { Button } from '../inputs';
-import { IconPlus, IconX } from '@tabler/icons';
-import GoalForm from './GoalForm';
-import GoalTable from './GoalTable';
-import { fetchCategory, fetchSingleGoal } from '../../infra';
+import React, { useEffect, useState } from "react";
+import { Modal, PageHeader, PageLayout } from "../common";
+import { Button } from "../inputs";
+import { IconPlus, IconX } from "@tabler/icons";
+import GoalForm from "./GoalForm";
+import GoalTable from "./GoalTable";
+import { fetchCategory, fetchSingleGoal } from "../../infra";
 
 const Goal = () => {
   const [isExpenseFormOpen, setIsExpenseFormOpen] = useState(false);
-  const [cat, setCat] = useState()
+  const [cat, setCat] = useState();
   const [type, setType] = useState();
   const [modelID, setModelID] = useState();
   const [val, setVal] = useState();
   useEffect(() => {
-    fetchCategory()
-      .then(setCat)
+    fetchCategory().then(setCat);
   }, []);
 
   useEffect(() => {
-    (
-      async () => {
-        if (modelID) {
-          const val = await fetchSingleGoal(modelID);
-          setVal(val);
-          setIsExpenseFormOpen(true)
-        }
+    (async () => {
+      if (modelID) {
+        const val = await fetchSingleGoal(modelID);
+        setVal(val);
+        setIsExpenseFormOpen(true);
       }
-    )()
-  }, [modelID])
+    })();
+  }, [modelID]);
 
   return (
     <>
       <PageHeader
-        title={('Set Budget')}
-        crumbs={[
-          { label: ('Add budget') },
-        ]}
+        title={"Set Budget"}
+        crumbs={[{ label: "Add budget" }]}
         action={
           <div className="flex mt-2">
-            <Button onClick={() => {
-              setIsExpenseFormOpen(true)
-            }} full="true" size="xs" kind="primary" className="mr-2"><IconPlus size="20px" className="mr-2" />Add Budget
+            <Button
+              onClick={() => {
+                setIsExpenseFormOpen(true);
+                setType("Create");
+                setVal(null);
+              }}
+              full="true"
+              size="xs"
+              kind="primary"
+              className="mr-2"
+            >
+              <IconPlus size="20px" className="mr-2" />
+              Add Budget
             </Button>
           </div>
         }
       />
-      <Modal isOpen={isExpenseFormOpen} onClose={() => { setIsExpenseFormOpen(false) }}>
+      <Modal
+        isOpen={isExpenseFormOpen}
+        onClose={() => {
+          setIsExpenseFormOpen(false);
+        }}
+      >
         <div className="mb-2 flex justify-between">
           <div>
             <h3 className="text-lg font-bold">Budget</h3>
           </div>
-          <button onClick={() => { setIsExpenseFormOpen(false) }}>
+          <button
+            onClick={() => {
+              setIsExpenseFormOpen(false);
+            }}
+          >
             <IconX size={15} />
           </button>
         </div>
@@ -60,13 +74,12 @@ const Goal = () => {
       </Modal>
 
       <PageLayout>
-        {cat &&
+        {cat && (
           <GoalTable setType={setType} setModelID={setModelID} cat={cat} />
-        }
+        )}
       </PageLayout>
     </>
+  );
+};
 
-  )
-}
-
-export default Goal
+export default Goal;
--- src/components/goals/GoalTable.jsx
+++ src/components/goals/GoalTable.jsx
@@ -1,51 +1,53 @@
-import React, { useMemo } from 'react';
-import DataTable from '../tables/DataTable';
-import { deleteGoal, fetchGoal } from '../../infra';
-import { PlainButton } from '../inputs';
-import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid';
-import toastify from '../../utils/toast';
+import React, { useMemo } from "react";
+import DataTable from "../tables/DataTable";
+import { deleteGoal, fetchGoal } from "../../infra";
+import { PlainButton } from "../inputs";
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+import toastify from "../../utils/toast";
 
 const GoalTable = ({ cat, setType, setModelID }) => {
   toastify();
   const columns = useMemo(
     () => [
       {
-        Header: 'Id',
-        accessor: 'id',
+        Header: "Id",
+        accessor: "id",
       },
       {
-        Header: 'Budget Name',
-        accessor: 'name',
+        Header: "Budget Name",
+        accessor: "name",
       },
       {
-        Header: 'Targt Budget Amount',
-        accessor: 'targetAmount',
+        Header: "Targt Budget Amount",
+        accessor: "targetAmount",
       },
       {
-        Header: 'Category',
+        Header: "Category",
         Cell: ({ row: { original } }) => {
-          const category = cat?.data?.find(c => c.id === original.categoryId);
-          return <span>{category ? category.title : ''}</span>;
+          const category = cat?.data?.find((c) => c.id === original.categoryId);
+          return <span>{category ? category.title : ""}</span>;
         },
       },
 
       {
-        Header: 'Actions',
-        id: 'actions',
+        Header: "Actions",
+        id: "actions",
         Cell: ({ row: { original } }) => {
           return (
             <div className="space-x-3">
-              <PlainButton onClick={() => {
-                setType('Update')
-                setModelID(original.id)
-              }}>
+              <PlainButton
+                onClick={() => {
+                  setType("Update");
+                  setModelID(original.id);
+                }}
+              >
                 <PencilAltIcon className="w-5 h-5" />
               </PlainButton>
               <PlainButton
                 onClick={() => {
-                  if (window.confirm('Are you sure?')) {
-                    deleteGoal(original.id).then(data => {
-                      localStorage.setItem('toastMessage', data.message);
+                  if (window.confirm("Are you sure?")) {
+                    deleteGoal(original.id).then((data) => {
+                      localStorage.setItem("toastMessage", data.message);
                       window.location.reload();
                     });
                   }
@@ -58,7 +60,7 @@ const GoalTable = ({ cat, setType, setModelID }) => {
         },
       },
     ],
-    [],
+    []
   );
 
   return (
@@ -66,7 +68,7 @@ const GoalTable = ({ cat, setType, setModelID }) => {
       <DataTable
         columns={columns}
         apiRequest={fetchGoal}
-        placeholder={'Search by invoice number, name, amount...'}
+        placeholder={"Search by invoice number, name, amount..."}
       />
     </div>
   );
